9d73a6e5e09ad5ad9fa4811cdba669bdf73ab865
Laura Decroix <laura.decroix@ece-france.com>
2018-12-22 01:26:59 +0100

    beginning

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/contact'
+  get 'pages/about'
   root to: 'pages#home'
+  get 'home', to: 'pages#home', as: :home
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'about', to: 'pages#about', as: :about
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
