d529deeb0076f163ce264283cf71d4d23c48a7d1
Laura Decroix <laura.decroix@ece-france.com>
2018-12-24 00:20:09 +0100

    education and changes on expereinces pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   def about
   end
 
-  def skills
+  def education
   end
 
   def experiences
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'home', to: 'pages#home', as: :home
   get 'contact', to: 'pages#contact', as: :contact
   get 'about', to: 'pages#about', as: :about
-  get 'skills', to: 'pages#skills', as: :skills
+  get 'education', to: 'pages#education', as: :education
   get 'experiences', to: 'pages#experiences', as: :experiences
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
